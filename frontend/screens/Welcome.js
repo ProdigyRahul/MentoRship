f250abf83467e3d80374e884725d92b234eff60b
Rahul Mistry <22cs042@charusat.edu.in>
2024-03-09 17:45:38 +0530

    Countries States and Cities

--- frontend/screens/Welcome.js
+++ frontend/screens/Welcome.js
@@ -5,11 +5,16 @@ import {
   TextInput,
   ScrollView,
   TouchableOpacity,
+  StyleSheet,
 } from "react-native";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Icon from "react-native-vector-icons/FontAwesome5";
 import { Picker } from "@react-native-picker/picker";
+import { Dropdown } from "react-native-element-dropdown";
+import axios from "axios";
 
+const API_KEY = "SWhsTnlyUnI3TjFRcDV1ZE1XVFFoNlIzZ3NMTkcwaUtsZGNZNTdBNQ==";
+const BASE_URL = "https://api.countrystatecity.in/v1";
 const NavigationLine = ({ active }) => (
   <View
     style={{
@@ -23,7 +28,98 @@ const NavigationLine = ({ active }) => (
 
 export default function Welcome() {
   // Countries/States/Cities
+  const [countryData, setCountryData] = useState([]);
+  const [stateData, setStateData] = useState([]);
+  const [cityData, setCityData] = useState([]);
+  const [country, setCountry] = useState(null);
+  const [state, setState] = useState(null);
+  const [city, setCity] = useState(null);
+  const [countryName, setCountryName] = useState(null);
+  const [stateName, setStateName] = useState(null);
+  const [cityName, setCityName] = useState(null);
+  const [isFocus, setIsFocus] = useState(false);
+  useEffect(() => {
+    var config = {
+      method: "get",
+      url: `${BASE_URL}/countries`,
+      headers: {
+        "X-CSCAPI-KEY": API_KEY,
+      },
+    };
 
+    axios(config)
+      .then((response) => {
+        console.log(JSON.stringify(response.data));
+        var count = Object.keys(response.data).length;
+        let countryArray = [];
+        for (var i = 0; i < count; i++) {
+          countryArray.push({
+            value: response.data[i].iso2,
+            label: response.data[i].name,
+          });
+        }
+        setCountryData(countryArray);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const handleState = (countryCode) => {
+    var config = {
+      method: "get",
+      url: `${BASE_URL}/countries/${countryCode}/states`,
+      headers: {
+        "X-CSCAPI-KEY": API_KEY,
+      },
+    };
+
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+        var count = Object.keys(response.data).length;
+        let stateArray = [];
+        for (var i = 0; i < count; i++) {
+          stateArray.push({
+            value: response.data[i].iso2,
+            label: response.data[i].name,
+          });
+        }
+        setStateData(stateArray);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  const handleCity = (countryCode, stateCode) => {
+    var config = {
+      method: "get",
+      url: `${BASE_URL}/countries/${countryCode}/states/${stateCode}/cities`,
+      headers: {
+        "X-CSCAPI-KEY": API_KEY,
+      },
+    };
+
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+        var count = Object.keys(response.data).length;
+        let cityArray = [];
+        for (var i = 0; i < count; i++) {
+          cityArray.push({
+            value: response.data[i].id,
+            label: response.data[i].name,
+          });
+        }
+        setCityData(cityArray);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  // Others
   const [selectedGender, setSelectedGender] = useState("");
   const [selectedRole, setSelectedRole] = useState(null);
   const [selectedParticipation, setSelectedParticipation] = useState(null);
@@ -205,7 +301,7 @@ export default function Welcome() {
             </Picker>
           </View>
           <Text style={{ marginTop: 15, fontWeight: 300 }}>Country *</Text>
-          <TextInput
+          <View
             style={{
               backgroundColor: "#F1F1F3",
               width: "100%",
@@ -214,10 +310,44 @@ export default function Welcome() {
               marginTop: 15,
               paddingHorizontal: 20,
               borderColor: "#D9D9D9",
+              justifyContent: "center",
             }}
-          ></TextInput>
+          >
+            <Dropdown
+              placeholderStyle={{
+                fontSize: 16,
+              }}
+              selectedTextStyle={{
+                fontSize: 16,
+              }}
+              inputSearchStyle={{
+                height: 40,
+                fontSize: 16,
+              }}
+              iconStyle={{
+                width: 20,
+                height: 20,
+              }}
+              data={countryData}
+              search
+              maxHeight={300}
+              labelField="label"
+              valueField="value"
+              placeholder={!isFocus ? "Select country" : "..."}
+              searchPlaceholder="Search..."
+              value={country}
+              onFocus={() => setIsFocus(true)}
+              onBlur={() => setIsFocus(false)}
+              onChange={(item) => {
+                setCountry(item.value);
+                handleState(item.value);
+                setCountryName(item.label);
+                setIsFocus(false);
+              }}
+            />
+          </View>
           <Text style={{ marginTop: 15, fontWeight: 300 }}>State *</Text>
-          <TextInput
+          <View
             style={{
               backgroundColor: "#F1F1F3",
               width: "100%",
@@ -226,10 +356,44 @@ export default function Welcome() {
               marginTop: 15,
               paddingHorizontal: 20,
               borderColor: "#D9D9D9",
+              justifyContent: "center",
             }}
-          ></TextInput>
+          >
+            <Dropdown
+              placeholderStyle={{
+                fontSize: 16,
+              }}
+              selectedTextStyle={{
+                fontSize: 16,
+              }}
+              inputSearchStyle={{
+                height: 40,
+                fontSize: 16,
+              }}
+              iconStyle={{
+                width: 20,
+                height: 20,
+              }}
+              data={stateData}
+              search
+              maxHeight={300}
+              labelField="label"
+              valueField="value"
+              placeholder={!isFocus ? "Select state" : "..."}
+              searchPlaceholder="Search..."
+              value={state}
+              onFocus={() => setIsFocus(true)}
+              onBlur={() => setIsFocus(false)}
+              onChange={(item) => {
+                setState(item.value);
+                handleCity(country, item.value);
+                setStateName(item.label);
+                setIsFocus(false);
+              }}
+            />
+          </View>
           <Text style={{ marginTop: 15, fontWeight: 300 }}>City *</Text>
-          <TextInput
+          <View
             style={{
               backgroundColor: "#F1F1F3",
               width: "100%",
@@ -238,8 +402,41 @@ export default function Welcome() {
               marginTop: 15,
               paddingHorizontal: 20,
               borderColor: "#D9D9D9",
+              justifyContent: "center",
             }}
-          ></TextInput>
+          >
+            <Dropdown
+              placeholderStyle={{
+                fontSize: 16,
+              }}
+              selectedTextStyle={{
+                fontSize: 16,
+              }}
+              inputSearchStyle={{
+                height: 40,
+                fontSize: 16,
+              }}
+              iconStyle={{
+                width: 20,
+                height: 20,
+              }}
+              data={cityData}
+              search
+              maxHeight={300}
+              labelField="label"
+              valueField="value"
+              placeholder={!isFocus ? "Select city" : "..."}
+              searchPlaceholder="Search..."
+              value={city}
+              onFocus={() => setIsFocus(true)}
+              onBlur={() => setIsFocus(false)}
+              onChange={(item) => {
+                setCity(item.value);
+                setCityName(item.label);
+                setIsFocus(false);
+              }}
+            />
+          </View>
         </View>
         <Text
           style={{
