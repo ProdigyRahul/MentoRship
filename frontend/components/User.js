50f905bce5a6c9c8792b887e60c4ec5f67a7e5cf
Rahul Mistry <22cs042@charusat.edu.in>
2024-04-07 11:48:32 +0530

    Backend and Explore Page

--- frontend/components/User.js
+++ frontend/components/User.js
@@ -1,76 +1,60 @@
-import { StyleSheet, Text, View, Pressable, Image } from "react-native";
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext } from "react";
+import { View, Text, Pressable, Image, StyleSheet } from "react-native";
 import { UserType } from "../UserContext";
 
-const User = ({ item }) => {
-  const { userId, setUserId } = useContext(UserType);
-  const [requestSent, setRequestSent] = useState(false);
-  const [friendRequests, setFriendRequests] = useState([]);
-  const [userFriends, setUserFriends] = useState([]);
-  useEffect(() => {
-    const fetchFriendRequests = async () => {
-      try {
-        const response = await fetch(
-          `https://api.rahulmistry.in/friend-requests/sent/${userId}`
+const User = ({ item, category }) => {
+  const { userId } = useContext(UserType);
+
+  const renderActionButton = () => {
+    switch (category) {
+      case "Friends":
+        return (
+          <Pressable
+            style={{
+              backgroundColor: "#09A1F6",
+              padding: 10,
+              width: 105,
+              borderRadius: 6,
+            }}
+          >
+            <Text style={{ textAlign: "center", color: "white" }}>Message</Text>
+          </Pressable>
         );
-
-        const data = await response.json();
-        if (response.ok) {
-          setFriendRequests(data);
-        } else {
-          console.log("error", response.status);
-        }
-      } catch (error) {
-        console.log("error", error);
-      }
-    };
-
-    fetchFriendRequests();
-  }, []);
-
-  useEffect(() => {
-    const fetchUserFriends = async () => {
-      try {
-        const response = await fetch(
-          `https://api.rahulmistry.in/friends/${userId}`
+      case "Request Sent":
+        return (
+          <Pressable
+            style={{
+              backgroundColor: "gray",
+              padding: 10,
+              width: 105,
+              borderRadius: 6,
+            }}
+          >
+            <Text style={{ textAlign: "center", color: "white", fontSize: 13 }}>
+              Request Sent
+            </Text>
+          </Pressable>
         );
-
-        const data = await response.json();
-
-        if (response.ok) {
-          setUserFriends(data);
-        } else {
-          console.log("error retrieving user friends", response.status);
-        }
-      } catch (error) {
-        console.log("Error message", error);
-      }
-    };
-
-    fetchUserFriends();
-  }, []);
-  const sendFriendRequest = async (currentUserId, selectedUserId) => {
-    try {
-      const response = await fetch(
-        "https://api.rahulmistry.in/friend-request",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ currentUserId, selectedUserId }),
-        }
-      );
-
-      if (response.ok) {
-        setRequestSent(true);
-      }
-    } catch (error) {
-      console.log("error message", error);
+      case "Add Friend":
+        return (
+          <Pressable
+            style={{
+              backgroundColor: "#09A1F6",
+              padding: 10,
+              borderRadius: 6,
+              width: 105,
+            }}
+          >
+            <Text style={{ textAlign: "center", color: "white", fontSize: 13 }}>
+              Add Friend
+            </Text>
+          </Pressable>
+        );
+      default:
+        return null;
     }
   };
-  console.log("friend requests sent", friendRequests);
-  console.log("user friends", userFriends);
+
   return (
     <Pressable
       style={{ flexDirection: "row", alignItems: "center", marginVertical: 10 }}
@@ -83,53 +67,14 @@ const User = ({ item }) => {
             borderRadius: 25,
             resizeMode: "cover",
           }}
-          source={{ uri: item.image }}
+          source={{ uri: item?.image }}
         />
       </View>
 
       <View style={{ marginLeft: 12, flex: 1 }}>
         <Text style={{ fontWeight: "bold" }}>{item?.name}</Text>
       </View>
-      {userFriends.includes(item._id) ? (
-        <Pressable
-          style={{
-            backgroundColor: "#09A1F6",
-            padding: 10,
-            width: 105,
-            borderRadius: 6,
-          }}
-        >
-          <Text style={{ textAlign: "center", color: "white" }}>Message</Text>
-        </Pressable>
-      ) : requestSent ||
-        friendRequests.some((friend) => friend._id === item._id) ? (
-        <Pressable
-          style={{
-            backgroundColor: "gray",
-            padding: 10,
-            width: 105,
-            borderRadius: 6,
-          }}
-        >
-          <Text style={{ textAlign: "center", color: "white", fontSize: 13 }}>
-            Request Sent
-          </Text>
-        </Pressable>
-      ) : (
-        <Pressable
-          onPress={() => sendFriendRequest(userId, item._id)}
-          style={{
-            backgroundColor: "#09A1F6",
-            padding: 10,
-            borderRadius: 6,
-            width: 105,
-          }}
-        >
-          <Text style={{ textAlign: "center", color: "white", fontSize: 13 }}>
-            Add Friend
-          </Text>
-        </Pressable>
-      )}
+      {renderActionButton()}
     </Pressable>
   );
 };
--- frontend/screens/Explore.js
+++ frontend/screens/Explore.js
@@ -7,90 +7,161 @@ import {
   TouchableOpacity,
   StyleSheet,
   ActivityIndicator,
+  StatusBar,
+  Platform,
 } from "react-native";
 import { UserType } from "../UserContext";
 import axios from "axios";
 import User from "../components/User";
+import { LinearGradient } from "expo-linear-gradient";
+import { MaterialIcons } from "@expo/vector-icons";
 
 export default function Explore({ navigation }) {
   const { userId, setUserId } = useContext(UserType);
+  const [category, setCategory] = useState("Friends");
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
 
+  const fetchUsers = async () => {
+    let url = "";
+    switch (category) {
+      case "Friends":
+        url = `https://api.rahulmistry.in/friends/${userId}`;
+        break;
+      case "Request Sent":
+        url = `https://api.rahulmistry.in/friend-requests/sent/${userId}`;
+        break;
+      case "Add Friend":
+        url = `https://api.rahulmistry.in/users/${userId}`;
+        break;
+      default:
+        break;
+    }
+
+    try {
+      const response = await axios.get(url);
+      setUsers(response.data);
+      setLoading(false);
+    } catch (error) {
+      console.log("Error fetching users:", error);
+    }
+  };
+
   useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        setTimeout(async () => {
-          const response = await axios.get(
-            `https://api.rahulmistry.in/users/${userId}`
-          );
-          setUsers(response.data);
-          setLoading(false);
-        }, 500);
-      } catch (error) {
-        console.log("Error fetching users:", error);
-      }
-    };
-
     fetchUsers();
-  }, []);
+  }, [category]);
 
-  const navigateToMentorRequest = () => {
-    navigation.navigate("MentorRequest");
+  const handleCategoryChange = (selectedCategory) => {
+    setCategory(selectedCategory);
   };
 
   if (loading) {
     return (
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
+      <LinearGradient
+        colors={["#000000", "#007CB0"]}
+        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 0 }}
+        locations={[0.3, 1]}
       >
-        <ActivityIndicator size="large" color="#000000" />
-        <Text style={{ marginTop: 10, color: "#000000" }}>Please wait...</Text>
-      </View>
+        <ActivityIndicator size="large" color="#FFFFFF" />
+        <Text style={{ marginTop: 10, color: "#FFFFFF" }}>Please wait...</Text>
+      </LinearGradient>
     );
   }
 
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.header}>Mentorship Conversations</Text>
-      <View style={styles.separator}></View>
-      <View style={styles.userList}>
-        <FlatList
-          data={users}
-          renderItem={({ item }) => <User item={item} />}
-          keyExtractor={(item, index) => index.toString()}
-          contentContainerStyle={{ paddingBottom: 20 }}
-        />
+    <LinearGradient
+      colors={["#000000", "#007CB0"]}
+      style={{ flex: 1 }}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 0 }}
+      locations={[0.3, 1]}
+    >
+      <StatusBar barStyle="white-content" />
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          paddingHorizontal: 20,
+          marginTop: 45,
+        }}
+      >
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          style={{ marginRight: 15 }}
+        >
+          <MaterialIcons name="arrow-back" size={24} color="white" />
+        </TouchableOpacity>
+        <Text
+          style={{
+            fontSize: 25,
+            fontWeight: "bold",
+            color: "#FFFFFF",
+          }}
+        >
+          Explore
+        </Text>
       </View>
-    </SafeAreaView>
+      <View style={styles.friendsContainer}>
+        <View style={{ marginTop: 10, marginLeft: 10, padding: 10 }}>
+          <FlatList
+            horizontal
+            data={[
+              { id: "Friends", text: "Friends" },
+              { id: "Request Sent", text: "Request Sent" },
+              { id: "Add Friend", text: "Add Friend" },
+            ]}
+            renderItem={({ item }) => (
+              <TouchableOpacity
+                style={[
+                  styles.item,
+                  item.id === category && { backgroundColor: "#09A1F6" },
+                ]}
+                onPress={() => handleCategoryChange(item.id)}
+              >
+                <Text style={styles.text}>{item.text}</Text>
+              </TouchableOpacity>
+            )}
+            keyExtractor={(item) => item.id}
+          />
+        </View>
+        <View style={styles.userList}>
+          <FlatList
+            data={users}
+            renderItem={({ item }) => <User item={item} category={category} />}
+            keyExtractor={(item, index) => index.toString()}
+            contentContainerStyle={{ paddingBottom: 20 }}
+          />
+        </View>
+      </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#D9D9D9",
+    backgroundColor: "#fff",
   },
-  header: {
-    fontSize: 25,
-    fontWeight: "bold",
-    color: "#000000",
-    marginTop: 50,
-    textAlign: "center",
-  },
-  separator: {
-    width: "100%",
-    height: 1.5,
-    backgroundColor: "#D9D9D9",
-    marginTop: 10,
+  friendsContainer: {
+    flex: 1,
+    borderTopStartRadius: 50,
+    borderTopEndRadius: 50,
+    backgroundColor: "#FFFFFF",
+    marginTop: 20,
   },
   userList: {
-    flex: 1,
     padding: 10,
+  },
+  item: {
+    padding: 5,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  text: {
+    fontSize: 15,
+    color: "black",
   },
   buttonContainer: {
     marginBottom: 150,
